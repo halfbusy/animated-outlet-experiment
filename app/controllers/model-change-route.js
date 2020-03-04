d095245d93da619dbcd08512be98573bdc01b4ce
DanDan <halfbusy@ismyusername.github.com>
2020-03-03 18:06:44 -0800

    Duplicate model sprite insert and no orphan

--- app/controllers/model-change-route.js
+++ app/controllers/model-change-route.js
@@ -1,8 +1,34 @@
 import Controller from "@ember/controller";
+import { action } from "@ember/object";
+
+import opacity from "ember-animated/motions/opacity";
+import adjustColor from "ember-animated/motions/adjust-color";
+import adjustCss from "ember-animated/motions/adjust-css";
 
 export default class ModelChangeRouteController extends Controller {
   get color() {
     const color = 200 / this.model.id;
     return `background-color:rgb(255,${color},255);`;
+  }
+
+  @action
+  *transition({ duration, insertedSprites, removedSprites, receivedSprites }) {
+    yield;
+
+    console.log("insertedSprites", insertedSprites.length);
+    console.log("removedSprites", removedSprites.length);
+    console.log("receivedSprites", receivedSprites.length);
+
+    insertedSprites.forEach(sprite => {
+      opacity(sprite, { from: 0, to: 1, duration: duration });
+    });
+
+    receivedSprites.forEach(sprite => {
+      opacity(sprite, { from: 0, to: 1, duration: duration });
+    });
+
+    removedSprites.forEach(sprite => {
+      opacity(sprite, { from: 1, to: 0, duration: duration });
+    });
   }
 }
