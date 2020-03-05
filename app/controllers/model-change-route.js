87ea2eeda4ebcf80af67d20e3cd07fd289907f6b
DanDan <halfbusy@ismyusername.github.com>
2020-03-05 14:25:27 -0800

    Cleanup

--- app/controllers/model-change-route.js
+++ app/controllers/model-change-route.js
@@ -8,25 +8,4 @@ export default class ModelChangeRouteController extends Controller {
   get color() {
     return `background-color:rgb(${this.colors[this.model.id - 1]});`;
   }
-
-  @action
-  *transition({ duration, insertedSprites, removedSprites, receivedSprites }) {
-    yield;
-
-    console.log("insertedSprites", insertedSprites.length);
-    console.log("removedSprites", removedSprites.length);
-    console.log("receivedSprites", receivedSprites.length);
-
-    insertedSprites.forEach(sprite => {
-      opacity(sprite, { from: 0, to: 1, duration: duration });
-    });
-
-    receivedSprites.forEach(sprite => {
-      opacity(sprite, { from: 0, to: 1, duration: duration });
-    });
-
-    removedSprites.forEach(sprite => {
-      opacity(sprite, { from: 1, to: 0, duration: duration });
-    });
-  }
 }
